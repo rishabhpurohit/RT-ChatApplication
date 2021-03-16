5b008914aafbcaec7ffa0f587016c2be4b9c58be
Rishabh Purohit <rishabh.purohit123456789@gmail.com>
2021-03-16 21:41:45 +0530

    configured backend

--- public/app.js
+++ public/app.js
@@ -1,19 +1,61 @@
 const socket = io();
 const msgText = document.querySelector('#msg');
 const btnSend = document.querySelector('#btn-send');
-const chatBOx = document.querySelector('.chat-content');
-const msgText = document.querySelector('.message');
+const chatBox = document.querySelector('.chat-content');
+const displayMsg = document.querySelector('.message');
 
-let name;
+let name1;
 do{
-    name = prompt("Enter your name HERE!")
-}while(!name)
+    name1 = prompt("Enter your name HERE!")
+}while(!name1)
 
-document.querySelector('#your-name').textContent = name
+document.querySelector('#your-name').textContent = name1
 msgText.focus()
 
-btnSend.addEventListener('click' ()=>{
+btnSend.addEventListener('click', (e) => {
+    e.preventDefault()
     sendMsg(msgText.value)
+    msgText.value = '';
+    msgText.focus()
+    chatBox.scrollTop = chatBox.scrollHeight;
 })
+
+const sendMsg = message => {
+    let msg = {
+        user:name1,
+        message:message.trim()
+    }
+
+    display(msg,'you-message')
+
+    socket.emit('sendMessage',msg)
+}
+
+socket.on('sendToAll',msg=>{
+    display(msg,'other-message')
+    chatBox.scrollTop = chatBox.scrollHeight;
+})
+
+const display = (msg,type) =>{
+    const msgDiv = document.createElement('div')
+    let className = type
+    msgDiv.classList.add(className,'message-row')
+    let times = new Date().toLocaleDateString()
+
+    let innerText = `
+    <div class="message-title">
+        👻<span>${msg.user}</span>
+    </div>
+    <div class="message-text">
+        ${msg.message}                            
+    </div>
+    <div class="message-time">
+        ${times}
+    </div>
+    `;
+    msgDiv.innerHTML = innerText;
+    displayMsg.appendChild(msgDiv);
+}
+
 
 
--- server.js
+++ server.js
@@ -9,7 +9,11 @@ app.use(express.static(path.join(__dirname,'public')))
 const io = require('socket.io')(http)
 io.on('connection',socket=>{
     console.log('Connected Ready')
-    
+
+    socket.on('sendMessage', msg => {
+        //console.log(msg)
+        socket.broadcast.emit('sendToAll',msg)
+    })
 })
 
 const PORT = process.env.PORT || 3000
